251414ee6899995c7361cd2c492a6fb800ee1d9e
Anhnguyen <anhatnguyen123@gmail.com>
2024-11-17 21:14:33 +0700

    HW.sql

--- HW.sql
+++ HW.sql
@@ -1,30 +1,29 @@
 CREATE DATABASE ISTECH
 USE ISTECH
-CREATE TABLE ORDERS (
-    order_num DECIMAL(6,0),
-    order_date DATE,
-    order_quantity INT,
-    discount DECIMAL(5,4),
-    item_id CHAR(6),
-    customer_id CHAR(6),
-    store_id CHAR(6),
-    PRIMARY KEY (order_num)
-);
 
 CREATE TABLE ITEMS(
-item_id CHAR(6),
+item_id CHAR(6) PRIMARY KEY,
 item_name VARCHAR(40),
 item_price INT,
 )
-CREATE TABLE STORES
-(
-store_id CHAR(6),
+CREATE TABLE STORES (
+store_id CHAR(6) PRIMARY KEY,
 store_name VARCHAR(40),
 province NVARCHAR(40)
 )
-CREATE TABLE CUSTOMERS
-(customer_id CHAR(6),
+CREATE TABLE CUSTOMERS (
+customer_id CHAR(6) PRIMARY KEY,
 customer_name NVARCHAR(40),
 customer_country NVARCHAR(40),
 customer_phone VARCHAR(17),
 grade INT)
+
+CREATE TABLE ORDERS (
+    order_num DECIMAL(6,0) PRIMARY KEY,
+    order_date DATE,
+    order_quantity INT,
+    discount DECIMAL(5,4),
+    item_id CHAR(6) foreign key references ITEMS (item_id), 
+    customer_id CHAR(6) foreign key references CUSTOMERS (customer_id),
+    store_id CHAR(6) foreign key references STORES (store_id),
+);
